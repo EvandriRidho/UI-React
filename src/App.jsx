2b1fca46ef71d52dd827d7e3fa1e1d1be5b143af
unknown <evandriridho555@gmail.com>
2024-07-11 10:57:52 +0700

    Update Belajar Import dan Export

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Gallery from './Gallery.jsx';
+import Profile from './Profile.jsx';
+
 export default function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Gallery/>
+      <Profile/>
+    </div>
+  );
 }
-
